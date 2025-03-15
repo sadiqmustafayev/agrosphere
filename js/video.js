5ee197a70a51b2ff0b9c11b3828c8749100a6def
sadiqmustafayev <mustafaevsadiq22@gmail.com>
2025-03-16 02:18:07 +0400

    agrosphere

--- js/video.js
+++ js/video.js
@@ -1,14 +1,22 @@
 document.addEventListener("DOMContentLoaded", function () {
   var videoModal = document.getElementById("videoModal");
-  var videoFrame = document.getElementById("video");
+  var videoFrame = document.getElementById("videoFrame"); // ID düzəldildi
 
   videoModal.addEventListener("show.bs.modal", function (event) {
       var button = event.relatedTarget;
-      var videoSrc = button.getAttribute("data-src").replace("youtu.be/", "www.youtube.com/embed/").split("?")[0];
-      videoFrame.src = videoSrc;
+      var videoSrc = button.getAttribute("data-src");
+
+      // YouTube linkini embed formasına çevirmək
+      if (videoSrc.includes("watch?v=")) {
+          videoSrc = videoSrc.replace("watch?v=", "embed/");
+      } else if (videoSrc.includes("youtu.be/")) {
+          videoSrc = videoSrc.replace("youtu.be/", "www.youtube.com/embed/");
+      }
+
+      videoFrame.src = videoSrc; // Yeni link iframe-ə yüklənir
   });
 
   videoModal.addEventListener("hidden.bs.modal", function () {
-      videoFrame.src = "";
+      videoFrame.src = ""; // Modal bağlananda videonu dayandır
   });
 });
